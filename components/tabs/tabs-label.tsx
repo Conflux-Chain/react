ff56c417c55b467cccff4763860ebb822c0bcdee
guichi <zgc910619@gmail.com>
2020-08-19 10:14:09 +0800

    fix(tabs): fix merge conflict

--- components/tabs/tabs-label.tsx
+++ components/tabs/tabs-label.tsx
@@ -17,10 +17,7 @@ const Label: LabelCpt = ({ label, varient, status, colors }) => {
    */
   const [width, ref] = useDOMDimension<HTMLDivElement>('offsetWidth')
   const extra = useExtraStyle(varient, status)
-<<<<<<< HEAD
   const { layout, expressiveness } = useTheme()
-=======
->>>>>>> Nav
 
   return (
     <div style={{ ...colors, ...extra, width: width ? width : '' }} ref={ref} className="label">
