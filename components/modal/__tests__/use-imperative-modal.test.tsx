ec383c9c8bd9afcc258afa13ff834a934d6d0f6c
Angelia <greenfish84@126.com>
2020-08-19 15:49:17 +0800

    test(modal): add coverage

--- components/modal/__tests__/use-imperative-modal.test.tsx
+++ components/modal/__tests__/use-imperative-modal.test.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react'
 import { mount, ReactWrapper } from 'enzyme'
-import { Modal } from 'components'
-import { updateWrapper } from 'tests/utils'
+import { Modal, Button } from 'components'
+import { nativeEvent, updateWrapper } from 'tests/utils'
 
 export const expectModalIsOpened = (wrapper: ReactWrapper) => {
   expect(wrapper.find('.content').length).not.toBe(0)
@@ -33,5 +33,59 @@ describe('useModalHandle', () => {
     wrapper.setProps({ show: false })
     await updateWrapper(wrapper, 500)
     expectModalIsClosed(wrapper)
+  })
+})
+
+describe('closeModal', () => {
+  it('should close directly when open is underfined', async () => {
+    const MockModal: React.FC<{ show?: boolean }> = ({ show }) => {
+      const { setVisible, ref } = Modal.useModalHandle()
+      useEffect(() => {
+        if (show !== undefined) setVisible(show)
+      }, [show])
+      return (
+        <Modal ref={ref} onClose={() => setVisible(false)}>
+          <Modal.Title>Modal</Modal.Title>
+        </Modal>
+      )
+    }
+
+    const wrapper = mount(<MockModal />)
+    wrapper.setProps({ show: true })
+    await updateWrapper(wrapper, 500)
+    wrapper.find('.backdrop').simulate('click', nativeEvent)
+    await updateWrapper(wrapper, 500)
+    expectModalIsClosed(wrapper)
+  })
+})
+
+describe('getVisible', () => {
+  it('should get current visible', async () => {
+    let log = ''
+    jest.spyOn(console, 'log').mockImplementation(msg => (log = msg))
+    const MockModal: React.FC<{ show?: boolean }> = ({ show }) => {
+      const { setVisible, getVisible, ref } = Modal.useModalHandle()
+      useEffect(() => {
+        if (show !== undefined) setVisible(show)
+        setTimeout(() => console.log(getVisible()), 0)
+      }, [show])
+      // useEffect(() => {
+      //   console.log(getVisible())
+      // }, [show])
+      return (
+        <Modal ref={ref} onClose={() => setVisible(false)}>
+          <Modal.Title>Modal</Modal.Title>
+          <Modal.Content>{getVisible()}</Modal.Content>
+        </Modal>
+      )
+    }
+
+    const wrapper = mount(<MockModal />)
+    wrapper.setProps({ show: true })
+    await updateWrapper(wrapper, 500)
+    expect(log).toEqual(true)
+    wrapper.setProps({ show: false })
+    await updateWrapper(wrapper, 500)
+    expect(log).toEqual(false)
   })
 })
